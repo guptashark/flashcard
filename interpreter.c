5a41c5f49d9166237edac4dcccaef44f57920f5e
Aishwary Gupta <guptashark@protonmail.com>
2019-11-01 18:27:44 -0400

    Implement several static config and pack fns
    In the interpreter, we'll have static fn's to
    be able to do things internally, that
    shouldn't be exposed to the main function or
    any other module.
    
    interpreter.c:
     * create static functions that deal with configs
     * create static functions that deal with packs.

--- interpreter.c
+++ interpreter.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "pack.h"
 #include "interpreter.h"
 
 #include "fc_shell.h"
@@ -9,7 +10,50 @@
 struct interpreter {
 
 	int var;
+	struct fc_shell *fcs;
 };
+
+static
+void
+interpreter_create_config
+(struct interpreter *inptr) {
+
+	(void)inptr;
+	// TODO make it possible to create a config wherever.
+	char * home_val = getenv("HOME");
+	char config_path [128];
+
+	strcpy(config_path, home_val);
+	strcat(config_path, "/.flashcardrc");
+
+	FILE *config = fopen(config_path, "w+");
+
+	// fill the file with some default values
+	// TODO fill with useful information!
+	fputs("# This is the flashcard config", config);
+	fclose(config);
+}
+
+static
+void
+interpreter_load_pack
+(struct interpreter *inptr, FILE *pack_path) {
+	(void)inptr;
+	(void)pack_path;
+}
+
+static
+void
+interpreter_use_pack
+(struct interpreter *inptr, struct pack *p) {
+
+	struct card *c;
+
+	for(size_t i = 0; i < pack_size(p); i++) {
+		c = pack_card_at(p, i);
+		fc_shell_insert(inptr->fcs, card_get_question(c));
+	}
+}
 
 struct interpreter *
 interpreter_ctor(void) {
@@ -25,7 +69,7 @@ interpreter_run
 (struct interpreter *inptr) {
 	(void)inptr;
 
-	// check to see if the config file exists in user home. 
+	// check to see if the config file exists in user home.
 	char * home_val = getenv("HOME");
 	char config_path [128];
 
@@ -35,8 +79,15 @@ interpreter_run
 	FILE *config = fopen(config_path, "r");
 	if (NULL == config) {
 		printf("No config found\n");
+		interpreter_create_config(inptr);
 	} else {
 		printf("Opened the config file!\n");
+	}
+
+	// create a pack!
+	if(false) {
+		interpreter_load_pack(NULL, NULL);
+		interpreter_use_pack(NULL, NULL);
 	}
 
 	printf("finished running\n");
